259d8ef0976a7b740e9b453a5dafd442732f5e0b
yokozhem <kozhemyakova.elena@gmail.com>
2023-02-03 14:08:49 -0500

    HW 3 done

--- Lesson2/020/Program.cs
+++ Lesson2/020/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-
-// 20. Дано число. Проверить кратно ли оно 7 и 23
+﻿// 20. Дано число. Проверить кратно ли оно 7 и 23
 
 int N=46;
 
---
+++ Lesson3/030/Program.cs
@@ -0,0 +1,12 @@
+﻿// 30. Вывести на экран таблицу квадратов чисел от 1 до N
+
+
+
+            Console.WriteLine("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i + "   " + (i * i));
+            }
+        
+ 
