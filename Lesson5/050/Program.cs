3315d40507b2203e4eac949fecd69d5984fbf02f
yokozhem <kozhemyakova.elena@gmail.com>
2023-02-10 11:56:18 -0500

    HW5 all done

---
+++ Lesson5/050/Program.cs
@@ -0,0 +1,14 @@
+﻿// 50. Выяснить являются ли три числа сторонами треугольника
+
+int a,b,c;
+a=int.Parse(Console.ReadLine());
+b=int.Parse(Console.ReadLine());
+c=int.Parse(Console.ReadLine());
+    if(a+b>c && a+c>b && c+b>a)
+{
+    System.Console.WriteLine("Ваши числа являются сторонами треугольника");
+}
+else
+{
+    System.Console.WriteLine("Увы, но нет.");
+}
---
+++ Lesson5/053/Program.cs
@@ -0,0 +1,19 @@
+﻿//53. Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+
+double k1,k2,b1,b2;
+System.Console.Write("Введите значение k1: ");
+k1=double.Parse(Console.ReadLine());
+System.Console.Write("Введите значение b1: ");
+b1=double.Parse(Console.ReadLine());
+System.Console.Write("Введите значение k2: ");
+k2=double.Parse(Console.ReadLine());
+System.Console.Write("Введите значение b2: ");
+b2=double.Parse(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+
+System.Console.WriteLine($"Точка пересеычения прямых имеет координаты {x} {y}");
+
