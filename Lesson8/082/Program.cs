da41b9c83f556326a99af259199f34f767e66770
yokozhem <kozhemyakova.elena@gmail.com>
2023-02-20 21:49:27 -0500

    All tasks 8  completed

---
+++ Lesson8/082/Program.cs
@@ -0,0 +1,12 @@
+﻿// 82. C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+System.Console.WriteLine("Введите текст с разделением точкой");
+string input=Console.ReadLine();
+int index = input.IndexOf(".");
+
+
+if (index == -1) {
+            Console.WriteLine("Точка не найдена");
+        } else 
+        {
+            Console.WriteLine($"Количество символов до точки: {index}");
+        }
---
+++ Lesson8/088/Program.cs
@@ -0,0 +1,13 @@
+﻿// 88. Заменить все вхождения подстроки w в строке st на строку v. 
+// Вывести на экран первоначальную строку и конечную строку
+
+
+ string st = "This is a test string.";
+        string w = "test";
+        string v = "example";
+
+        Console.WriteLine($"Исходная строка: {st}");
+
+        string newString = st.Replace(w, v);
+
+        Console.WriteLine($"Конечная строка: {newString}");
